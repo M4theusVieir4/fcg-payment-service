101d29e05abccf1a701c7e99b45758e18c775458
M4theusVieir4 <vieiramatheus72@gmail.com>
2025-09-28 16:34:15 -0300

    refactor: update project, namespace, and solution naming

---
+++ FCGPaymentService/FCG.PaymentService/Program.cs
@@ -0,0 +1,30 @@
+using FCG.PaymentService.Infra.Ioc;
+using FCG.PaymentService.Infra.Ioc.Observability;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddOpenTelemetryInfra(
+    serviceName: "FCGPaymentService",
+    serviceVersion: "1.0"
+);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddOpenApi();
+builder.Services.AddInfrastructure(builder.Configuration);
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+app.Run();
--- FCGPaymentService/FCGPaymentService.Infra.Data/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace FCGPaymentService.Infra.Data
-{
-    public class Class1
-    {
-
-    }
-}
